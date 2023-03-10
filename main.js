d32c1b3490544d05d78276ee1524336ee48a9309
casio25 <mishakolomiets355@gmail.com>
2023-03-10 21:02:53 +0200

    backend

---
+++ main.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer((req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Methods", "GET, POST, UPDATE");
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    const url = req.url;
+    let body ="";
+
+    if (req.method === "POST"){
+        if (url === "/data"){
+            console.log("post");
+            req.on("data", (data) =>{
+                body += data.toString();
+            });
+            req.on("end", () => {
+                const newData = JSON.parse(body);
+                const dataUpdate = JSON.parse(fs.readFileSync("data.txt"));
+                dataUpdate.push(newData);
+                fs.writeFileSync("data.txt", JSON.stringify(dataUpdate));
+            });
+            res.write(JSON.stringify(fs.readFileSync("data.txt")));
+            res.end("POST check");
+        }
+    }else if(req.method === "GET"){
+        if (url === "/data"){
+            const weatherData = fs.readFileSync("data.txt", "utf-8");
+            res.end(weatherData);
+        }else if (weatherData.status !== 200){
+            res.end("Cannot find weather data")
+        }
+    }
+}).listen(4000);
